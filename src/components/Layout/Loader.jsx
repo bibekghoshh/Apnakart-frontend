e90267a94e57e486ca9847e85a2a4f0d9c9c80ed
bibekghosh <bibek7478@gmail.com>
2023-06-13 23:41:00 +0530

    rm lottie

--- src/components/Layout/Loader.jsx
+++ src/components/Layout/Loader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Lottie from "react-lottie";
+import "./Loader.css"
+// import Lottie from "react-lottie";
 import animationData from "../../Assests/animations/24151-ecommerce-animation.json";
 
 const Loader = () => {
@@ -13,7 +14,8 @@ const Loader = () => {
   };
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <Lottie options={defaultOptions} width={300} height={300} />
+      {/* <Lottie options={defaultOptions} width={300} height={300} /> */}
+      <section options={defaultOptions} class="paytm-loader"></section>
     </div>
   );
 };
--- src/pages/OrderSuccessPage.jsx
+++ src/pages/OrderSuccessPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
-import Lottie from "react-lottie";
+import "./../components/Layout/Loader.css"
+// import Lottie from "react-lottie";
 import animationData from "../Assests/animations/107043-success.json";
 
 const OrderSuccessPage = () => {
@@ -25,7 +26,8 @@ const Success = () => {
   };
   return (
     <div>
-      <Lottie options={defaultOptions} width={300} height={300} />
+      {/* <Lottie options={defaultOptions} width={300} height={300} /> */}
+      <section options={defaultOptions} class="paytm-loader"></section>
       <h5 className="text-center mb-14 text-[25px] text-[#000000a1]">
         Your order is successful 😍
       </h5>
