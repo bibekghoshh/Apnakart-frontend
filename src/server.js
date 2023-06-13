1c17c9953d33ecee5194ec5c7d2682e77fa41618
bibekghosh <bibek7478@gmail.com>
2023-06-14 00:33:06 +0530

    backend url added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://apnakart.onrender.com/api/v2";
+export const server = "https://apnakart-backend.onrender.com/api/v2";
 
-export const backend_url = "https://apnakart.onrender.com/"
+export const backend_url = "https://apnakart-backend.onrender.com/"
 
 // export const backend_url = "http://localhost:8000/"
